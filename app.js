d87bc8d959f8a0457f61a7905416396a56768e00
Robert Reilley <rpreilley@gmail.com>
2016-06-17 15:10:31 -0500

    finished

--- app.js
+++ app.js
@@ -5,23 +5,38 @@ var yosay = require('yosay');
 
 
 // 1. Welcome the user to the CLI program
-console.log(yosay("Welcome to NASA! Enter a Date! (MM-DD)"));
+console.log(yosay("Welcome to NASA!\n1. ISS \n 2. Picture of the Day\n Choose a number to proceed.\n3. Random Asteroid Data."));
 
-// 2. Prompt the user for keywords for subreddits
+
 prompt.start();
-prompt.get(['date'], function (err, result) {
-  console.log('You chose ' + result.date + '!');
-  if(result.date === 'ISS') {
+prompt.get(['choice'], function (err, result) {
+  console.log('You chose ' + result.choice + '!');
+  if(result.choice === '1') {
     open('http://www.ustream.tv/channel/live-iss-stream')
   }
-  else {
-  // A. Make a GET request to the search endpoint to find NASA stuff
-  request('https://api.nasa.gov/planetary/apod?date=2015-' + result.date + '&api_key=2ifGrri2F4HDQlsPeldjGOJuedJbkZRX4dgIt7OT', function (error, response, site) {
-    if (!error && response.statusCode == 200) {
-      var parsedSite = JSON.parse(site);
-      var image = parsedSite.hdurl;
-      open(image);
-    }
-  })
-}
+  if(result.choice == '2') {
+  console.log(yosay("Picture of the Day! Type a date (YYYY-MM-DD) for a picture!"))
+  prompt.start();
+  prompt.get(['date'], function(err, number) {
+    request('https://api.nasa.gov/planetary/apod?date=' + number.date + '&api_key=2ifGrri2F4HDQlsPeldjGOJuedJbkZRX4dgIt7OT', function (error, response, site) {
+      if (!error && response.statusCode == 200) {
+        var parsedSite = JSON.parse(site);
+        var image = parsedSite.hdurl;
+        open(image);
+        }
+      })
+    })
+  }
+  if(result.choice == '3') {
+  console.log(yosay("Asteroid information!."))
+    request('https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=2ifGrri2F4HDQlsPeldjGOJuedJbkZRX4dgIt7OT', function (error, response, site) {
+      if (!error && response.statusCode == 200) {
+        var parsedSite = JSON.parse(site);
+        var asteroid = parsedSite.nasa_jpl_url;
+        open(asteroid);
+        }
+      })
+
+  }
 });
+
